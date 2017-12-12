fe73065c0383aaafd084ebf98d1850fd96651382
Jorge Ulises Useche Cuellar <naturalmentejorge@gmail.com>
2017-12-12 09:30:32 -0500

    Código inicial con flask

---
+++ main.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+from flask import Flask, flash, redirect, render_template, \
+    request, url_for
+
+app = Flask(__name__)
+app.secret_key = 'some_secret'
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+def call_software():
+    from subprocess import call
+    #call(["ls", "-l"])
+    call(["gnome-terminal"])
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'admin' or \
+                request.form['password'] != 'secret':
+            error = 'Invalid credentials'
+        else:
+            flash('You were successfully logged in')
+            return redirect(url_for('index'))
+    call_software()
+    return 'Hello, World!'
+    #return render_template('login.html', error=error)
