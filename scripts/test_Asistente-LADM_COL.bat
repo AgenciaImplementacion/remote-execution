dd0eb4c4c547f5527b9110725ce057078289b00f
Jorge Useche <naturalmentejorge@gmail.com>
2018-03-03 09:48:25 -0500

    Se actualiza script para soportar release oficial QGIS3

--- scripts/test_Asistente-LADM_COL.bat
+++ scripts/test_Asistente-LADM_COL.bat
@@ -11,14 +11,14 @@ call o4w_env.bat
 call qt5_env.bat
 call py3_env.bat
 @echo off
-path %OSGEO4W_ROOT%\apps\qgis-dev\bin;C:\Program Files\Git\cmd;C:\Users\aimplementacion\AppData\Local\Programs\Python\Python36\Scripts;%PATH%
-set QGIS_PREFIX_PATH=%OSGEO4W_ROOT:\=/%/apps/qgis-dev
+path %OSGEO4W_ROOT%\apps\qgis\bin;C:\Program Files\Git\cmd;C:\Users\aimplementacion\AppData\Local\Programs\Python\Python36\Scripts;%PATH%
+set QGIS_PREFIX_PATH=%OSGEO4W_ROOT:\=/%/apps/qgis
 set GDAL_FILENAME_IS_UTF8=YES
 rem Set VSI cache to be used as buffer, see #6448
 set VSI_CACHE=TRUE
 set VSI_CACHE_SIZE=1000000
-set QT_PLUGIN_PATH=%OSGEO4W_ROOT%\apps\qgis-dev\qtplugins;%OSGEO4W_ROOT%\apps\qt5\plugins
-set PYTHONPATH=%OSGEO4W_ROOT%\apps\qgis-dev\python;C:\Users\aimplementacion\AppData\Local\Programs\Python\Python36\Lib\site-packages;%PYTHONPATH%
+set QT_PLUGIN_PATH=%OSGEO4W_ROOT%\apps\qgis\qtplugins;%OSGEO4W_ROOT%\apps\qt5\plugins
+set PYTHONPATH=%OSGEO4W_ROOT%\apps\qgis\python;C:\Users\aimplementacion\AppData\Local\Programs\Python\Python36\Lib\site-packages;%PYTHONPATH%
 
 ECHO Se inician pruebas
 REM xvfb-run xbnose2-3
