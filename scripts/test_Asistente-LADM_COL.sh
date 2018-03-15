3c0f1f737cfb0d11b12583703605b0894e8f4c03
Jorge Ulises <naturalmentejorge@gmail.com>
2018-03-15 17:54:37 -0500

    Se actualizan ordenes de ejecución de tests en linux

--- scripts/test_Asistente-LADM_COL.sh
+++ scripts/test_Asistente-LADM_COL.sh
@@ -3,4 +3,7 @@ echo 'Este archivo imita al batch para probar en linux'
 echo 'Se inician pruebas'
 cd /home/jorge/workspace/Asistente-LADM_COL
 git pull origin master
+cd asistente_ladm_col
+make
+cd ..
 nose2-3
