a0ebc231c3cce546ece9e48121f2926159400e0f
Jorge Useche <naturalmentejorge@gmail.com>
2018-05-24 17:03:21 -0500

    Se agrega nueva version de qgis

--- scripts/test_Asistente-LADM_COL.bat
+++ scripts/test_Asistente-LADM_COL.bat
@@ -20,7 +20,7 @@ rem https://qgis.org/downloads/QGIS-OSGeo4W-3.0.0-3-Setup-x86_64.exe # new
 rem C:\Users\aimplementacion\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup
 rem https://answers.microsoft.com/es-es/windows/forum/windows_10-other_settings-winpc/windows-10-programar-una-tarea-cómo-programar/dd0be19b-0365-4407-90f2-426c014d4da1
 @echo off
-cd C:\Program Files\QGIS 3.0\bin
+cd C:\Program Files\QGIS 3.1\bin
 call o4w_env.bat
 call qt5_env.bat
 call py3_env.bat
@@ -32,7 +32,7 @@ rem Set VSI cache to be used as buffer, see #6448
 set VSI_CACHE=TRUE
 set VSI_CACHE_SIZE=1000000
 set QT_PLUGIN_PATH=%OSGEO4W_ROOT%\apps\qgis\qtplugins;%OSGEO4W_ROOT%\apps\qt5\plugins
-set PYTHONPATH=%OSGEO4W_ROOT%\apps\qgis\python;C:\Program Files\QGIS 3.0\apps\qgis\python\plugins;C:\Users\aimplementacion\AppData\Roaming\QGIS\QGIS3\profiles\default\python\plugins;C:\Users\aimplementacion\AppData\Local\Programs\Python\Python36\Lib\site-packages;%PYTHONPATH%
+set PYTHONPATH=%OSGEO4W_ROOT%\apps\qgis\python;C:\Program Files\QGIS 3.1\apps\qgis\python\plugins;C:\Users\aimplementacion\AppData\Roaming\QGIS\QGIS3\profiles\default\python\plugins;C:\Users\aimplementacion\AppData\Local\Programs\Python\Python36\Lib\site-packages;%PYTHONPATH%
 
 ECHO Se termina configuracion de ambiente de pruebas
 REM xvfb-run xbnose2-3
