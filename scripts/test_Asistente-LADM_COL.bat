2e26b71088deb57d47884613b7a462a57dcdce23
Jorge Useche <naturalmentejorge@gmail.com>
2018-07-03 11:54:34 -0500

    Upgrating to qgis 3.3

--- scripts/test_Asistente-LADM_COL.bat
+++ scripts/test_Asistente-LADM_COL.bat
@@ -20,11 +20,13 @@ rem https://qgis.org/downloads/QGIS-OSGeo4W-3.0.0-3-Setup-x86_64.exe # new
 rem C:\Users\aimplementacion\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup
 rem https://answers.microsoft.com/es-es/windows/forum/windows_10-other_settings-winpc/windows-10-programar-una-tarea-cómo-programar/dd0be19b-0365-4407-90f2-426c014d4da1
 @echo off
-cd C:\Program Files\QGIS 3.1\bin
+set QGIS_INSTALLATION_PATH=C:\Program Files\QGIS 3.3
+cd %QGIS_INSTALLATION_PATH%\bin
 call o4w_env.bat
 call qt5_env.bat
 call py3_env.bat
 @echo off
+echo %QGIS_INSTALLATION_PATH% is equals to %OSGEO4W_ROOT% ?
 path %OSGEO4W_ROOT%\apps\qgis-dev\bin;C:\Program Files\Git\cmd;C:\Users\aimplementacion\AppData\Local\Programs\Python\Python36\Scripts;%PATH%
 set QGIS_PREFIX_PATH=%OSGEO4W_ROOT:\=/%/apps/qgis-dev
 set GDAL_FILENAME_IS_UTF8=YES
@@ -32,7 +34,7 @@ rem Set VSI cache to be used as buffer, see #6448
 set VSI_CACHE=TRUE
 set VSI_CACHE_SIZE=1000000
 set QT_PLUGIN_PATH=%OSGEO4W_ROOT%\apps\qgis-dev\qtplugins;%OSGEO4W_ROOT%\apps\Qt5\plugins
-set PYTHONPATH=%OSGEO4W_ROOT%\apps\qgis-dev\python;C:\Program Files\QGIS 3.1\apps\qgis-dev\python\plugins;C:\Users\aimplementacion\AppData\Roaming\QGIS\QGIS3\profiles\default\python\plugins;C:\Users\aimplementacion\AppData\Local\Programs\Python\Python36\Lib\site-packages;%PYTHONPATH%
+set PYTHONPATH=%OSGEO4W_ROOT%\apps\qgis-dev\python;%OSGEO4W_ROOT%\apps\qgis-dev\python\plugins;C:\Users\aimplementacion\AppData\Roaming\QGIS\QGIS3\profiles\default\python\plugins;C:\Users\aimplementacion\AppData\Local\Programs\Python\Python36\Lib\site-packages;%PYTHONPATH%
 
 ECHO Se termina configuracion de ambiente de pruebas
 REM xvfb-run xbnose2-3
